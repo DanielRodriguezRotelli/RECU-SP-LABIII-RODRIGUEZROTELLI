605b13c2486cf64febe855f99c392447d4d5f68b
Daniel Rodiguez Rotelli <daniel.rodriguez.rotelli@gmail.com>
2023-12-01 20:36:13 -0300

    entrega

--- RodriguezRotelli-SPPLABIII3D/client/js/localStorage.js
+++
@@ -1,8 +0,0 @@
-export function LeerData(key) {
-  return JSON.parse(localStorage.getItem(key)) || [];
-}
-
-export function GuardarData(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
--- RodriguezRotelli-SPPLABIII3D/client/js/personaje.js
+++
@@ -1,8 +0,0 @@
-export class Personaje{
-    constructor(id, nombre, miedo)
-    {
-        this.id = id;
-        this.nombre = nombre;
-        this.miedo = miedo;
-    }
-}
